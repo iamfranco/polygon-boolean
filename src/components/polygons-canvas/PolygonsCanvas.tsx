b0fd8998f5b92f798f78242c707cc24b1e84b6ac
iamfranco <kinfaifrancochan@gmail.com>
2023-09-24 09:53:23 +0100

    Create blank PolygonCanvas component, without test

---
+++ src/components/polygons-canvas/PolygonsCanvas.tsx
@@ -0,0 +1,9 @@
+import './PolygonsCanvas.css'
+
+const PolygonsCanvas = () => {
+  return (
+    <canvas id='polygons-canvas' width={300} height={400}></canvas>
+  )
+}
+
+export default PolygonsCanvas
